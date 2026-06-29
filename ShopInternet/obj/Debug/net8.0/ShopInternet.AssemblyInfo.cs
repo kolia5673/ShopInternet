8ea7fd795b743a7ced6e43ec6ffc698f617196ad
Віталій Шовкун <vetalshovkun@gmail.com>
2026-06-29 20:07:42 +0300

    Add User Identity

--- ShopInternet/obj/Debug/net8.0/ShopInternet.AssemblyInfo.cs
+++ ShopInternet/obj/Debug/net8.0/ShopInternet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopInternet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eef1bdfbfeb2066d50ea3981b737b6a9ea8d7cd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8665733be8e5b4cd1aa2b9ecb07ba050d46af768")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopInternet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopInternet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
