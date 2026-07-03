d27bba86d5068e5a7d35f83f6eba139928919c16
Віталій Шовкун <vetalshovkun@gmail.com>
2026-07-03 19:56:36 +0300

    Add api /api/product

--- ShopInternet/obj/Debug/net8.0/ShopInternet.AssemblyInfo.cs
+++ ShopInternet/obj/Debug/net8.0/ShopInternet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopInternet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8665733be8e5b4cd1aa2b9ecb07ba050d46af768")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ea7fd795b743a7ced6e43ec6ffc698f617196ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopInternet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopInternet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
